bedb7a90ac8cca6bf84f0c5fabc9de76eb9b1114
Frank Milburn <fmilburn3@users.noreply.github.com>
2015-12-13 22:43:18 -0800

    Create accounts.h

---
+++ accounts.h
@@ -0,0 +1,8 @@
+// This header file contains the account information needed to connect a CC3200 to a wifi
+// network and then to a MarineTraffic account
+
+#define WIFI_SSID       "yourSSID"               // your network name also called SSID in quotes
+#define WIFI_PASSWORD   "1234567890ABCDEF"       // your network password in quotes
+
+#define SERVER_IP       "5.9.207.224"            // IP address for MarineTraffic in quotes
+#define SERVER_PORT     1234                     // your MarineTraffic port, integer
